475f5c1600e804c95aba72e9ead699d2a7a3b24e
Anton Cataldi <antoncataldi@gmail.com>
2017-03-02 13:02:39 -0500

    First commit

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+// function swap(a,b){
+//   return [b,a]
+// }
+
+function bubbleSort(arr){
+  for(var j = arr.length - 1; j > 0; j--){
+    for(var i = 0; i < j; i++){
+      if(arr[i] > arr[i+1]){
+        var swapped = bubbleSort.prototype.swap(arr[i],arr[i+1])
+        arr[i] = swapped[0]
+        arr[i+1] = swapped[1]
+      }
+    }
+  }
+  return arr
+}
+
+bubbleSort.prototype.swap = function(a,b){
+  return [b,a]
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,33 @@
+describe('Bubble Sort', function(){
+
+  var bubble = bubbleSort([75,34,2,45,3,0,97]);
+
+  beforeEach(function (){
+    spyOn(bubble,'sort').and.callThrough();
+  });
+
+  it('counts how many swaps happened', function(){
+    var bub = bubbleSort([4,3])
+    expect( bub.prototype.swap.calls.count() ).toEqual( 1 );
+  });
+
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles an array of size 1', function(){
+    expect( bubbleSort([1]) ).toEqual( [1] );
+  });
+
+  it('handles an array of size 2', function(){
+    expect( bubbleSort([4,3]) ).toEqual( [3,4] );
+  });
+
+  it('handles a large array', function(){
+    expect( bubbleSort([75,34,2,45,3,0,97]) ).toEqual( [0,2,3,34,45,75,97] );
+  });
+
+  it('handles a large array w/ negative numbers', function(){
+    expect( bubbleSort([75,34,2,-12,45,3,0,97]) ).toEqual( [-12,0,2,3,34,45,75,97] );
+  });
+});
